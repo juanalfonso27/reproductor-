511677a5ba2e50904dbca8e36eb48a260305ed9d
juan <167249149+juanalfonso27@users.noreply.github.com>
2024-10-10 16:46:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,29 @@
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-storage-compat.js');
+const CACHE_NAME = 'video-cache-v1';
+const urlsToCache = [
+  '/videos/video1.mp4',
+  '/videos/video2.mp4',
+  '/videos/video3.mp4',
+  '/videos/video4.mp4'
+];
 
-// Configuración de Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBSMzKJYdRgA_JM9lX0pUhfvwkZ5nAOBRM",
-    authDomain: "reproductor-b1420.firebaseapp.com",
-    projectId: "reproductor-b1420",
-    storageBucket: "reproductor-b1420.appspot.com",
-    messagingSenderId: "60164868284",
-    appId: "1:60164868284:web:0f94e7e49eb6ee55bfe8b9"
-};
-
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('video-cache').then((cache) => {
-            const storageRef = firebase.storage().ref('videos'); // Referencia al directorio de videos
-
-            // Obtener la lista de archivos en el directorio de videos
-            return storageRef.listAll().then((result) => {
-                const urlPromises = result.items.map((itemRef) => {
-                    return itemRef.getDownloadURL().catch((error) => {
-                        console.error(`Error al obtener la URL de ${itemRef.name}:`, error);
-                    });
-                });
-
-                return Promise.all(urlPromises).then((urls) => {
-                    // Filtrar URLs undefined
-                    const validUrls = urls.filter(url => url !== undefined);
-                    return cache.addAll(validUrls);
-                });
-            }).catch((error) => {
-                console.error('Error al listar los archivos:', error);
-            });
-        })
-    );
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response; // Si el archivo está en caché, úsalo.
+        }
+        return fetch(event.request); // Si no está en caché, descárgalo.
+      })
+  );
 });
