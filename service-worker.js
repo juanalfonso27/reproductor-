a832353c6733494597adcdaec869a050587c18aa
juan <167249149+juanalfonso27@users.noreply.github.com>
2024-10-10 14:04:51 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,15 @@
+    <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-storage-compat.js"></script>
 
+        // Configuración de Firebase
+        const firebaseConfig = {
+            apiKey: "AIzaSyBSMzKJYdRgA_JM9lX0pUhfvwkZ5nAOBRM",
+            authDomain: "reproductor-b1420.firebaseapp.com",
+            projectId: "reproductor-b1420",
+            storageBucket: "reproductor-b1420.appspot.com",
+            messagingSenderId: "60164868284",
+            appId: "1:60164868284:web:0f94e7e49eb6ee55bfe8b9"
+        };
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
