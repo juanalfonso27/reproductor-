cecdbe8e37d1e9ccdd43261d01ebf4ff761ee855
juan <167249149+juanalfonso27@users.noreply.github.com>
2024-10-10 13:48:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,17 @@
+ const firebaseConfig = {
+            apiKey: "AIzaSyBSMzKJYdRgA_JM9lX0pUhfvwkZ5nAOBRM",
+            authDomain: "reproductor-b1420.firebaseapp.com",
+            projectId: "reproductor-b1420",
+            storageBucket: "reproductor-b1420.appspot.com",
+            messagingSenderId: "60164868284",
+            appId: "1:60164868284:web:0f94e7e49eb6ee55bfe8b9"
+        };
+
+        // Inicializar Firebase
+        firebase.initializeApp(firebaseConfig);
+        const storage = firebase.storage();
+
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('video-cache').then((cache) => {
